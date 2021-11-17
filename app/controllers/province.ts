a534650eaebea230986ff0781a18c67546d201d4
R.Phuwanat <r.phuwanat.dev@gmail.com>
2021-11-17 16:31:36 +0700

    Rsponse Test

--- app/controllers/province.ts
+++ app/controllers/province.ts
@@ -1,21 +1,13 @@
 import Controller from '../libs/controller';
 import {Http} from '../libs/http';
 import ProvinceData from '../models/province';
+import fs from 'fs';
+import path from 'path';
 
 export default class Province extends Controller {
 	all(http: Http) {
-		const provices: ProvinceData[] = [
-			{
-				id: 1,
-				nameTh: 'กรุงเทพมหานคร',
-				nameEn: 'Bangkok',
-			},
-			{
-				id: 2,
-				nameTh: 'สมุทรปราการ',
-				nameEn: 'Samut Prakan'
-			}
-		];
+		const file = fs.readFileSync(path.join(path.resolve()) + '/dist/provinces_new.json');
+		const provices: ProvinceData[] = JSON.parse(file.toString()); 
 		return http.response.json(provices);
 	}
 }
