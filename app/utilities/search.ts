ab30d11b983297e5edf0d5b7c557a723c0d87ac7
R.Phuwanat <r.phuwanat.dev@gmail.com>
2021-11-22 21:18:06 +0700

    types fix

--- app/utilities/search.ts
+++ app/utilities/search.ts
@@ -1,9 +1,14 @@
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  QuerySnapshot,
+} from "firebase/firestore";
 import { ProvinceDetail } from "../models/province";
 
-export async function requestHandler(fun: CallableFunction) {
+export async function requestFirebaseHandler(fun: CallableFunction) {
   try {
-    const data = await fun();
+    const data: QuerySnapshot = await fun();
     return { data, error: null };
   } catch (error) {
     return { data: null, error };
@@ -11,10 +16,11 @@ export async function requestHandler(fun: CallableFunction) {
 }
 
 export async function searchProvinces(queryText: string) {
-  const { data, error } = await requestHandler(
+  const { data, error } = await requestFirebaseHandler(
     async () => await getDocs(collection(getFirestore(), "provinces"))
   );
   if (error) return null;
+  if (!data) return null;
   const provinces: ProvinceDetail[] = data.docs.map(
     (doc) => doc.data() as ProvinceDetail
   );
